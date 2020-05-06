4ded73de9666a9d8dc993aa43ce4f0b36c5d806b
xukunn <xukunn@foxmail.com>
2020-05-06 09:43:29 +0800

    ..

--- example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
+++ example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
@@ -23,136 +23,128 @@ import java.util.Locale;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	private final String TAG = "MainActivity";
+  private final String TAG = "MainActivity";
 
-	BridgeWebView webView;
+  BridgeWebView webView;
 
-	Button button;
-	Button buttonAsync;
+  int RESULT_CODE = 0;
 
-	int RESULT_CODE = 0;
+  ValueCallback<Uri> mUploadMessage;
 
-	ValueCallback<Uri> mUploadMessage;
+  ValueCallback<Uri[]> mUploadMessageArray;
 
-	ValueCallback<Uri[]> mUploadMessageArray;
+  static class Location {
+    String address;
+  }
 
-    static class Location {
-        String address;
+  static class User {
+    String name;
+    Location location;
+    String testStr;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    webView = (BridgeWebView) findViewById(R.id.webView);
+
+    findViewById(R.id.button).setOnClickListener(this);
+    findViewById(R.id.buttonAsync).setOnClickListener(this);
+
+    webView.setWebChromeClient(new WebChromeClient() {
+
+      @SuppressWarnings("unused")
+      public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
+        this.openFileChooser(uploadMsg);
+      }
+
+      @SuppressWarnings("unused")
+      public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
+        this.openFileChooser(uploadMsg);
+      }
+
+      public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+        mUploadMessage = uploadMsg;
+        pickFile();
+      }
+
+      @Override
+      public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback,
+          FileChooserParams fileChooserParams) {
+        mUploadMessageArray = filePathCallback;
+        pickFile();
+        return true;
+      }
+    });
+
+    webView.addJavascriptInterface(new MainJavascriptInterface(webView.getCallbacks(), webView),
+        "android");
+    webView.setGson(new Gson());
+    webView.loadUrl("file:///android_asset/demo.html");
+
+    User user = new User();
+    Location location = new Location();
+    location.address = "SDU";
+    user.location = location;
+    user.name = "大头鬼";
+
+    webView.callHandler("functionInJs", new Gson().toJson(user), new OnBridgeCallback() {
+      @Override
+      public void onCallBack(String data) {
+        Log.d(TAG, "onCallBack: " + data);
+      }
+    });
+
+    webView.sendToWeb("hello");
+  }
+
+  public void pickFile() {
+    Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
+    chooserIntent.setType("image/*");
+    startActivityForResult(chooserIntent, RESULT_CODE);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    if (requestCode == RESULT_CODE) {
+      if (null == mUploadMessage && null == mUploadMessageArray) {
+        return;
+      }
+      if (null != mUploadMessage && null == mUploadMessageArray) {
+        Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+        mUploadMessage.onReceiveValue(result);
+        mUploadMessage = null;
+      }
+
+      if (null == mUploadMessage && null != mUploadMessageArray) {
+        Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+        if (result != null) {
+          mUploadMessageArray.onReceiveValue(new Uri[] { result });
+        }
+        mUploadMessageArray = null;
+      }
     }
+  }
 
-    static class User {
-        String name;
-        Location location;
-        String testStr;
-    }
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-        webView = (BridgeWebView) findViewById(R.id.webView);
-
-		button = (Button) findViewById(R.id.button);
-		button.setOnClickListener(this);
-		findViewById(R.id.buttonAsync).setOnClickListener(this);
-
-
-		webView.setWebChromeClient(new WebChromeClient() {
-
-			@SuppressWarnings("unused")
-			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
-				this.openFileChooser(uploadMsg);
-			}
-
-			@SuppressWarnings("unused")
-			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
-				this.openFileChooser(uploadMsg);
-			}
-
-			public void openFileChooser(ValueCallback<Uri> uploadMsg) {
-				mUploadMessage = uploadMsg;
-				pickFile();
-			}
-
-			@Override
-			public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-				mUploadMessageArray = filePathCallback;
-				pickFile();
-				return true;
-			}
-		});
-
-		webView.addJavascriptInterface(new MainJavascriptInterface(webView.getCallbacks(), webView), "android");
-		webView.setGson(new Gson());
-		webView.loadUrl("file:///android_asset/demo.html");
-
-        User user = new User();
-        Location location = new Location();
-        location.address = "SDU";
-        user.location = location;
-        user.name = "大头鬼";
-
-        webView.callHandler("functionInJs", new Gson().toJson(user), new OnBridgeCallback() {
-            @Override
-            public void onCallBack(String data) {
-				Log.d(TAG, "onCallBack: " + data);
+  @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+  @Override public void onClick(View v) {
+    if (R.id.button == v.getId()) {
+      webView.evaluateJavascript(String.format(Locale.CHINA, "javascript:%s", "syncFn(\"name\")"),
+          new ValueCallback<String>() {
+            @Override public void onReceiveValue(String s) {
+              Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
             }
-        });
+          });
+    } else if (R.id.buttonAsync == v.getId()) {
+      webView.callHandler("functionInJs", "java调用js方法的入参", new OnBridgeCallback() {
 
-        webView.sendToWeb("hello");
-
-	}
-
-	public void pickFile() {
-		Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
-		chooserIntent.setType("image/*");
-		startActivityForResult(chooserIntent, RESULT_CODE);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == RESULT_CODE) {
-			if (null == mUploadMessage && null == mUploadMessageArray){
-				return;
-			}
-			if(null!= mUploadMessage && null == mUploadMessageArray){
-				Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-				mUploadMessage.onReceiveValue(result);
-				mUploadMessage = null;
-			}
-
-			if(null == mUploadMessage && null != mUploadMessageArray){
-				Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-				if (result != null) {
-					mUploadMessageArray.onReceiveValue(new Uri[]{result});
-				}
-				mUploadMessageArray = null;
-			}
-
-		}
-	}
-
-	@RequiresApi(api = Build.VERSION_CODES.KITKAT) @Override
-	public void onClick(View v) {
-		if (button.equals(v)) {
-			webView.evaluateJavascript(String.format(Locale.CHINA,"javascript:%s","syncFn(\"name\")"), new ValueCallback<String>() {
-				@Override public void onReceiveValue(String s) {
-					Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
-				}
-			});
-
-		} else if (R.id.buttonAsync==v.getId()) {
-			webView.callHandler("functionInJs", "java调用js方法的入参", new OnBridgeCallback() {
-
-				@Override
-				public void onCallBack(String data) {
-					Log.i(TAG, "reponse data from js " + data);
-				}
-
-			});
-		}
-
-	}
-
+        @Override
+        public void onCallBack(String data) {
+          Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+        }
+      });
+    }
+  }
 }
